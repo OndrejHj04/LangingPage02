55b69a9e1a112bf56505b461ca396f77a8a9ece1
9inch <ondra.hajku@seznam.cz>
2022-01-02 16:22:09 +0100

    STAR WARS ⭐⚔️

---
+++ script.js
@@ -0,0 +1,31 @@
+let spans = document.querySelectorAll("span");
+
+move = (x) => {
+  let time = Number(x.id) * 1400;
+
+  setTimeout(() => {
+      x.style.display = "block"
+    let bottom = 0;
+    let size = 60;
+    measures();
+
+    function measures() {
+      bottom += 0.1;
+      size -= 0.05;
+      if (bottom < 100) {
+        setTimeout(() => {
+          measures();
+          x.style.bottom = bottom + "%";
+          x.style.fontSize = size + "px";
+        }, 20);
+      }
+    }
+  }, time);
+};
+
+for (let i = 0; i < spans.length; i++) {
+  spans[i].id = i;
+}
+spans.forEach((e) => {
+  move(e);
+});
