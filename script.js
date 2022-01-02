0f6843e8be9910d61770c63f9ae4b7c88cd304fc
9inch <ondra.hajku@seznam.cz>
2022-01-02 21:22:48 +0100

    other star wars stuff

--- script.js
+++ script.js
@@ -1,23 +1,24 @@
 let spans = document.querySelectorAll("span");
 
 move = (x) => {
-  let time = Number(x.id) * 1400;
-
+  let time = Number(x.id) * 1300; 
+  let time2 = 27
   setTimeout(() => {
       x.style.display = "block"
-    let bottom = 0;
+    let bottom = -10;
     let size = 60;
+
     measures();
 
     function measures() {
       bottom += 0.1;
-      size -= 0.05;
-      if (bottom < 100) {
+      size -= 0.040;
+      if (bottom <= 100) {
         setTimeout(() => {
-          measures();
-          x.style.bottom = bottom + "%";
-          x.style.fontSize = size + "px";
-        }, 20);
+          measures()
+          x.style.bottom = bottom + "%"
+          x.style.fontSize = size + "px"
+        }, time2);
       }
     }
   }, time);
