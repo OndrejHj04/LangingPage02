bd1f1979dc8bfb7b3de8b45e852dcfc87c8c8c1b
9inch <ondra.hajku@seznam.cz>
2022-01-03 15:30:28 +0100

    added arrows :sunglasses:

--- script.js
+++ script.js
@@ -1,26 +1,33 @@
 let spans = document.querySelectorAll("span");
-
+let img = document.querySelectorAll("img")
+let counter = 0;
 move = (x) => {
-  let time = Number(x.id) * 1300; 
-  let time2 = 27
+  let time = Number(x.id) * 1700; 
+  let time2 = 30
   setTimeout(() => {
       x.style.display = "block"
     let bottom = -10;
-    let size = 60;
+    let size = 0.9;
 
     measures();
 
     function measures() {
       bottom += 0.1;
-      size -= 0.035;
+      size -= 0.0006;
       if (bottom <= 100) {
         setTimeout(() => {
           measures()
           x.style.bottom = bottom + "%"
-          x.style.fontSize = size + "px";
+          x.style.transform = `scale(${size})`
         }, time2);
       }else{
         x.style.display = "none"
+        counter++
+        if(counter > 1){
+          img.forEach((e)=>{
+            e.style.display = "inline-block"
+          })
+        }
       }
     }
   }, time);
@@ -32,3 +39,16 @@ for (let i = 0; i < spans.length; i++) {
 spans.forEach((e) => {
   move(e);
 });
+document.body.addEventListener("click", ()=>{
+  img.forEach((e)=>{
+    e.style.display = "inline-block"
+  })
+})
+img.forEach((e)=>{
+  e.addEventListener("click", ()=>{
+    e.style.background = "red"
+  })
+})
+
+
+
