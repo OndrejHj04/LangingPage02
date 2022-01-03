a6aa6408705905858029d1850829e4eb22832795
9inch <ondra.hajku@seznam.cz>
2022-01-03 17:46:32 +0100

    add ons

--- script.js
+++ script.js
@@ -1,11 +1,13 @@
 let spans = document.querySelectorAll("span");
-let img = document.querySelectorAll("img")
+let img = document.querySelectorAll("img");
+let arrows = document.querySelector("#arrows");
 let counter = 0;
 move = (x) => {
-  let time = Number(x.id) * 1700; 
-  let time2 = 30
+  let time = Number(x.id) * 1700;
+  let time2 = 14;
+
   setTimeout(() => {
-      x.style.display = "block"
+    x.style.display = "block";
     let bottom = -10;
     let size = 0.9;
 
@@ -13,20 +15,21 @@ move = (x) => {
 
     function measures() {
       bottom += 0.1;
-      size -= 0.0006;
+      size -= 0.0007;
       if (bottom <= 100) {
         setTimeout(() => {
-          measures()
-          x.style.bottom = bottom + "%"
-          x.style.transform = `scale(${size})`
+          measures();
+          x.style.bottom = bottom + "%";
+          x.style.transform = `scale(${size})`;
+          time2 += 0.03;
         }, time2);
-      }else{
-        x.style.display = "none"
-        counter++
-        if(counter > 1){
-          img.forEach((e)=>{
-            e.style.display = "inline-block"
-          })
+      } else {
+        x.style.display = "none";
+        counter++;
+        if (counter > 1) {
+          img.forEach((e) => {
+            e.style.display = "inline-block";
+          });
         }
       }
     }
@@ -39,16 +42,34 @@ for (let i = 0; i < spans.length; i++) {
 spans.forEach((e) => {
   move(e);
 });
-document.body.addEventListener("click", ()=>{
-  img.forEach((e)=>{
-    e.style.display = "inline-block"
-  })
-})
-img.forEach((e)=>{
-  e.addEventListener("click", ()=>{
-    e.style.background = "red"
-  })
-})
+document.body.addEventListener("click", () => {
+  img.forEach((e) => {
+    e.style.display = "inline-block";
+  });
+});
+img.forEach((e) => {
+  e.addEventListener("click", () => {
+    if (e.id === "left") {
+      document.body.style.transition = "background-position-x 2000ms";
+      document.body.style.backgroundPositionX = "400px";
 
+      spans.forEach((e) => {
+        e.style.left = "100%";
+        e.style.transition = "left 3000ms";
+      });
+    }
 
+    if (e.id === "right") {
+      document.body.style.transition = "background-position-x 2000ms";
+      document.body.style.backgroundPositionX = "-400px";
 
+      spans.forEach((e) => {
+        e.style.right = "100%";
+        e.style.transition = "right 3000ms"; /*špatnej přechod :<*/
+      });
+    }
+  });
+});
+
+/*zmizení textu na konci obrazovky*/
+/*plynulost přechodu*/
